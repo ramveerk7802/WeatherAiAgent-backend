4a97406f9a37ad7b2a9305cb98d59f6e2997adf0
Ramveer <ramveerk7802@gmail.com>
2025-11-19 10:33:29 +0530

    initial project setup

---
+++ src/main/kotlin/com/example/WeatherAiAgent/WeatherAiAgentApplication.kt
@@ -0,0 +1,11 @@
+package com.example.WeatherAiAgent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeatherAiAgentApplication
+
+fun main(args: Array<String>) {
+	runApplication<WeatherAiAgentApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/WeatherAiAgent/WeatherAiAgentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.WeatherAiAgent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeatherAiAgentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
